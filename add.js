249a0333bb92541f21d9f2fbed937adc798a65e7
Tadhg Lewis <tadhg.lewis@outlook.com>
2020-10-20 19:57:53 +1100

    WIP

--- add.js
+++ add.js
@@ -36,13 +36,15 @@ const main = (ttTransform) => {
 
 module.exports = main;
 
-main();
-const octokit = new Octokit();
-const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+(async () => {
+  main();
+  const octokit = new Octokit();
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
-// See https://developer.github.com/v3/issues/#create-an-issue
-const { data } = await octokit.request("POST /repos/:owner/:repo/issues", {
-  owner,
-  repo,
-  title: "My test issue",
-});
+  // See https://developer.github.com/v3/issues/#create-an-issue
+  const { data } = await octokit.request("POST /repos/:owner/:repo/issues", {
+    owner,
+    repo,
+    title: "My test issue",
+  });
+})();
